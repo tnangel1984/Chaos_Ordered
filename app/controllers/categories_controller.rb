f8d21b8dd7ae3a05e2f5f21a3f0b9b53ced4eb00
tnangel1984 <tnangel1984123@gmail.com>
2018-05-05 13:41:53 -0400

    edited Categories routes & controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,23 @@
+class CategoriesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        render json: Category.all
+    end
+
+    def show
+        render json: Category.find(params["id"])
+    end
+
+    def create
+        render json: Category.create(params["category"])
+    end
+
+    def update
+        render json: Category.update(params["id"],params["category"])
+    end
+
+    def delete
+        render json: Category.delete(params["id"])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,17 @@ get '/articles/:id', to: 'articles#show'
 post '/articles', to: 'articles#create'
 put '/articles/:id', to: 'articles#update'
 delete '/articles/:id', to: 'articles#delete'
-# #
-# get '/users', :to 'users#index'
-# get '/users/:id', :to 'users#show'
-# post 'users', :to 'users#create'
-# put '/users/:id', :to 'users#update'
-# delete '/users:id' :to 'users#delete'
-#
-# get '/categories' :to 'categories#index'
-# get '/categories/:id' :to 'categories#show'
-# post '/categories' :to 'categories#create'
-# put '/categories/:id' :to 'categories#update'
-# delete '/categories/:id' :to 'categories#delete'
+
+get '/users', :to 'users#index'
+get '/users/:id', :to 'users#show'
+post 'users', :to 'users#create'
+put '/users/:id', :to 'users#update'
+delete '/users:id' :to 'users#delete'
+
+get '/categories' :to 'categories#index'
+get '/categories/:id' :to 'categories#show'
+post '/categories' :to 'categories#create'
+put '/categories/:id' :to 'categories#update'
+delete '/categories/:id' :to 'categories#delete'
 
 end
