b980ca83a6d09a7450f25c6f787421fbe5c91fa1
tnangel1984 <tnangel1984123@gmail.com>
2018-05-05 16:12:48 -0400

    rails: full crud drafted. NOT Tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,66 @@
 class User
-  attr_reader  :username, :password
+    attr_reader  :username, :password
 
-  DB= PG.connect(host:"localhost", port:5432, dbname:'chaos_ordered')
+      DB= PG.connect(host:"localhost", port:5432, dbname:'chaos_ordered')
 
-  def initialize (opts={})
-      @username = opts["username"]
-      @password = opts["password"]
-      # @articles = opts["article-id"].to_i
-      # @custom_categories = opts["custom_categories"]
-      # @category_id = opts["category_id"].to_i
+    def initialize (opts={})
+        @username = opts["username"]
+        @password = opts["password"]
+        # @articles = opts["article-id"].to_i
+        # @custom_categories = opts["custom_categories"]
+        # @category_id = opts["category_id"].to_i
 
-  end
+    end
 
-  def self.all
-    results= DB.exec(
-              <<-SQL
-                SELECT *
-                FROM users;
-              SQL
-    )
+    def self.all
+        results= DB.exec(
+                  <<-SQL
+                    SELECT *
+                    FROM users;
+                  SQL
+        )
+        return results.map { |result| User.new(result)}
+    end
 
-    return results.map { |result| User.new(result)
-   } end
 
+    def self.find(id)
+        results = DB.exec(
+            <<-SQL
+                SELECT * FROM users
+                WHERE id=#{id}
+            SQL
+        )
+        return User.new(results.first)
+    end
 
+    def self.create
+        results = DB.exec(
+            <<-SQL
+                INSERT INTO users (username, password)
+                VALUES('#{opts["username"]}', '#{opts["password"]}')
+                RETURNING id, username, passwords
+            SQL
+        )
+        return User.new(results.first)
+    end
+
+    def self.update(id, opts)
+        results=DB.exec(
+            <<-SQL
+                UPDATE users
+                SET username='#{opts["username"]}', password = #{opts["password"]}
+                RETURNING id, username, password
+            SQL
+        )
+        return User.new(results.first)
+    end
+
+    def self.delete
+        results = DB.exec(
+            <<-SQL
+                DELETE FROM users 
+                WHERE id=#{id}
+            SQL
+        )
+    end
 end
