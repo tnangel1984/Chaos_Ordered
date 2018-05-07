097cdb7d3b05c1f162112d3e8b386e7203e2869d
tnangel1984 <tnangel1984123@gmail.com>
2018-05-06 22:13:57 -0400

    react: article crud frame work started

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,53 @@
+
+
+
+class API extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = { articles:[], article:{}}
+    }
+
+
+    getArticles(){
+        fetch('/articles')
+        .then(response=>response.json())
+        .then(data=>this.setState({articles:data}))
+        .catch(error=>console.log(error))
+    }
+
+    getArticle(article){
+        this.setState({article:article})
+    }
+
+    createArticle(){
+
+    }
+
+    updateArticle(){
+
+    }
+
+    deleteArticle(id, article){
+
+    }
+
+
+    render(){
+        getArticles()
+        console.log(this.state);
+        return<div>
+            <h1>testAPI</h1>
+
+         </div>
+    }
+}
+
+
 ReactDOM.render(
     <div>
         <h1>testing</h1>
+        <API/>
         <Newsfeed/>
         <Category/>
         <Discussion/>
