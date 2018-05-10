88799a3aa140d85d9aa62f46b2b092a4b15ddaf6
tnangel1984 <tnangel1984123@gmail.com>
2018-05-10 18:17:40 -0400

    rails: appendend articles onto users in User model"

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User
         @username = opts["username"]
         @password = opts["password"]
         @saved_articles = opts["saved_articles"]
+
         # @articles = opts["article-id"].to_i
         # @custom_categories = opts["custom_categories"]
         # @category_id = opts["category_id"].to_i
@@ -29,14 +30,52 @@ class User
                     articles.summary,
                     articles.date_published
                     FROM users
-                     JOIN categories
+                    LEFT JOIN categories
                         ON users.id=categories.user_id
-                    JOIN articles
+                    LEFT JOIN articles
                         ON articles.id=categories.article_id;
                   SQL
         )
-        return results
+# ONE USER  MANY ARTICLES,  ONE LOCATION MANY PEOPLE
         # results.map { |result| User.new(result)}
+
+
+        saved_articles = []
+
+        users=[]
+        last_user_id = nil
+
+    #this first block re-creates an array of user objects, rather than using map.
+    #BECAUSE we need to embed a mechanism (IF stmt) to eliminate duplicates
+        results.each do |result|
+            if last_user_id != result["id"]  #comparison to eliminate duplicate users, if its a duplicate it won't be added to the array a second time.
+                users.push User.new(
+                    "username" => result["username"],
+                    "password"=> result["password"],
+                    "saved_articles" => []
+                )
+                last_user_id=result["id"]
+            end
+
+    # creates the MANY objects only if their id exists,
+            if result["article_id"]
+                new_article =Article.new({
+                    "title"=>result["title"],
+                    "author"=>result["author"],
+                    "url"=>result["url"],
+                    "image_url"=>result["image_url"],
+                    "source_name"=>result["source_name"],
+                    "summary"=>result["summary"],
+                    "date_published"=>result["date_published"]
+                })
+    #PUSHES the new many object onto the last item in the user array
+    #REMEMBER still in the each do loop, so the array is changing with every iteration, and the articles always belong to the last item added the the array, because they are take from the same record the user is on, otherwise a userid doesn't exist.
+                users.last.saved_articles.push(new_article)
+            end
+        end
+
+        return users
+
     end
 
 
